f39f4c2b26d02d12245b6d16a0ba4df2addd412e
qamar mudhafar <qamarmudhafar@yahoo.com>
2020-01-15 06:51:26 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+function findMatching(arr,string){
+    arr=arr.filter(function(f){
+        return f===string;
+    })
+    return arr;
+}
+
+function fuzzyMatch(arr,string){
+    arr.filter(function(f){
+        return f.indexof(string)===0;
+    })
+    return arr;
+}
+
+function matchName(arr,string){
+    arr=arr.filter(function(f){
+        return f.name==string;
+    })
+    return arr;
+}
