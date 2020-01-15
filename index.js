2f3686323567226d7f5ec8865a2d357dc27cf6b1
qamar mudhafar <qamarmudhafar@yahoo.com>
2020-01-15 06:52:48 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Code your solution here
+function findMatching(drivers,strToMatch){
+ return drivers.filter(s=>s.toLowerCase()==strToMatch.toLowerCase());
+}
+function fuzzyMatch(drivers,strToMatch){
+ return drivers.filter(s=>s.startsWith(strToMatch));
+}
+function matchName(drivers,strToMatch){
+ return drivers.filter(s=>s.name==strToMatch);
+}
